4ad6c385882c549cffc3c24b597821541a87b8e1
Mateus Alencar Ferreira <86336670+ferreiramateusalencar@users.noreply.github.com>
2024-12-16 19:49:24 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "unidades/comprimento/comprimento.h"
+#include "unidades/massa/massa.h"
+#include "unidades/volume/volume.h"
+#include "unidades/temperatura/temperatura.h"
+#include "unidades/velocidade/velocidade.h"
+#include "unidades/potencia/potencia.h"
+#include "unidades/area/area.h"
+#include "unidades/tempo/tempo.h"
+#include "unidades/dados/dados.h"
+
+int main(){
+    return 0;
+}
