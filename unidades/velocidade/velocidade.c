309d38382a1e4202382389d0259bc9422735c5a7
Leo-Luz-code <leonardoluz768@gmail.com>
2024-12-17 22:16:47 -0300

    feat: add velocidade conversion

--- unidades/velocidade/velocidade.c
+++ unidades/velocidade/velocidade.c
@@ -1,1 +1,11 @@
 #include "velocidade.h"
+
+void converter_velocidade(float metros_por_segundo)
+{
+    float km_por_hora = metros_por_segundo * 3.6;
+    float milhas_por_hora = metros_por_segundo * 2.23694;
+
+    printf("Velocidade:\n");
+    printf("%.2f m/s = %.2f km/h\n", metros_por_segundo, km_por_hora);
+    printf("%.2f m/s = %.2f mph\n", metros_por_segundo, milhas_por_hora);
+}
--- unidades/velocidade/velocidade.h
+++ unidades/velocidade/velocidade.h
@@ -1,6 +1,9 @@
 #ifndef VELOCIDADE_H
 #define VELOCIDADE_H
 
-// protótipos...
+#include <stdio.h>
+#include <stdlib.h>
+
+void converter_velocidade(float metros_por_segundo);
 
 #endif
